7b1f07625edaa4b43ba15164df25f8ef9f79c479
ashishdutta007 <ashish.dutta050@gmail.com>
2017-01-07 12:51:59 +0530

    final changes

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-//using express module
+//Using express module
 var express = require("express");
-//instance of the express server object
+//Using ipaddr.js module
+var ipaddr = require("ipaddr.js");
+//Instance of the express server object
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use('/api/whoami', function(request, response) {
-	console.log(request);
+//To server a static 'index.html' page
+app.use(express.static(__dirname + '/views'));
+
+//Middleware(specific path request handler) for request-headder-parser
+//Express req and res is wrapper around node's req and res
+app.use('/api/whoami/', function(request, response) {
+    request.on('error', function(error) {
+        return console.log("error: ", error);
+    });
+    response.on('error', function(error) {
+        return console.log("error: ", error);
+    });
+    //To get the ip of the requestor device
+    var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress || request.ip;
+    //To get the language set in the client browser
+    var lang = request.headers['accept-language'].split(",")[0];
+    //To get the host OS of the client
+    var os = request.headers['user-agent'].split("(")[1].split(")")[0];
+    response.json({
+        "ipaddress": ip,
+        "language": lang,
+        "hostOS": os
+    });
 });
 
 app.listen(port, function() {
-	console.log("Port running on ", port);
+    console.log("App server running on ", port);
 });
